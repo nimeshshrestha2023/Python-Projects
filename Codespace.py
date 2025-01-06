b7de0ed868c9d6efb769806f80754e719d9cbcaa
Nimesh Shrestha <140165562+nimeshshrestha2023@users.noreply.github.com>
2025-01-06 21:46:14 +0545

    Codespace.py

--- Codespace.py
+++ Codespace.py
@@ -1,1 +1,41 @@
+ What is Python? How does it works? Who developed it? why is it highly recommended today? and what are it's application?
+ -  Python is a high-level, versatile, and interpreted programming language known for its simplicity and readability which is Created by 
+Guido van Rossum and first released in 1991, which was named as 'Python' as inspired by the then popular show 
+"Monty Python ", Python has become one of the most popular programming languages in the world.
+  Why Learn Python?
+- Beginner-friendly and widely used in the industry.
+- High demand in job markets for roles like data scientists, software developers, and AI specialists.
+- Extensive learning resources and an active support community.
+  
 
+  Note : Make sure to follow the indentation rule in python, Indentation rule is highly sensative in python.
+
+  
+
+# Program to print the message "Hello future developers".
+print("Hello future developers")
+
+#program to input the value and then display it.
+  int num = int(input("Enter number: "))
+  print(f"The number you entered is {num}")
+
+#program to display the the following message.
+      Hello <your_name>,
+                Congratulation, you finally learn the basic python.
+                
+Code:
+    name = input("Enter the name: ")
+    print(f"Hello {name},\n Congratulation, you finally learn the basic python."
+
+#program to input any two numbers and calcualte it's addition.
+
+Code:
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print(f"The addition of {num1} and {num2} is ",num1+num2)   
+
+
+
+
+     Contents on progress ...
